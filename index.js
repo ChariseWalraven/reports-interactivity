54f84d344aa7f7b580a85cd28ae60e9a726ae66e
Charise Walraven <charise.walraven@kb.nl, charise.walraven@b-synergy.com>
2021-05-12 12:25:50 +0200

    Adds code to make error types collapsible; removes old library

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const __SETTINGS = {
-    searchable: true,
-    fixedHeight: true,
-}
-
-const errorsManualQATable = new simpleDatatables.DataTable("table[data-errors-manual-qa]", __SETTINGS);
-const errorsAutoQATable = new simpleDatatables.DataTable("table[data-errors-auto-qa]", __SETTINGS);
-const objectFilesTable = new simpleDatatables.DataTable("table[data-object-level]", __SETTINGS);
-const objectLevelTable = new simpleDatatables.DataTable("table[data-object-files-level]", __SETTINGS);
+(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const objectFilesLevelTable = document.querySelector('table[data-object-files-level]')
+        const tBodies = objectFilesLevelTable.querySelectorAll('tbody')
+        
+        tBodies.forEach(tBody => {
+            tBody.querySelectorAll('[class^="toggle-"]').forEach(toggle => {
+                toggle.addEventListener('click', _ => toggleGroup(tBody))
+            })
+        
+            const numErrors = tBody.querySelectorAll('tr').length
+            tBody.querySelector('.error-count').innerText = `(${numErrors})`
+        })
+        
+        console.log({ objectFilesLevelTable, tBodies })
+        
+        function toggleGroup(tBodyElement) {
+            if (tBodyElement.classList.contains('collapsed')) {
+                // if the group is hidden, show it
+                console.log({ message: 'Showing group', tBodyElement })
+                // show error group
+                tBodyElement.classList.remove('collapsed');
+                // hide 'show' toggle button
+                tBodyElement.querySelector('.toggle-show').classList.add('hidden')
+                // show 'hide' toggle button
+                tBodyElement.querySelector('.toggle-hide').classList.remove('hidden')
+            } else {
+                // otherwise hide it
+                console.log({ message: 'Hiding group', tBodyElement })
+                // show error group
+                tBodyElement.classList.add('collapsed');
+                // hide 'show' toggle button
+                tBodyElement.querySelector('.toggle-show').classList.remove('hidden')
+                // show 'hide' toggle button
+                tBodyElement.querySelector('.toggle-hide').classList.add('hidden')
+            }
+        }
+    })
+})()
